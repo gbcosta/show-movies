1f2a6ba36ccdedc83166c1bced3bfb0a425050e7
gbcostasant@gmail.com <gbcostasant@gmail.com>
2024-06-04 23:01:57 -0300

    add github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "tailwindcss"
-import autoprefixer from 'autoprefixer'
+import autoprefixer from "autoprefixer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "./",
+    build:{
+        outDir: "build",
+    },
     css:{
         postcss:{
             plugins:[tailwindcss(), autoprefixer()]
