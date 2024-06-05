4bda1ccdd0c9865d058ba9102eca41719b81399f
gbcostasant@gmail.com <gbcostasant@gmail.com>
2024-06-05 00:07:33 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "tailwindcss"
 import autoprefixer from "autoprefixer"
-
+import dotenv from 'dotenv';
 // https://vitejs.dev/config/
+
+dotenv.config()
 export default defineConfig({
     plugins: [react()],
     base: "./",
@@ -14,6 +16,9 @@ export default defineConfig({
         postcss:{
             plugins:[tailwindcss(), autoprefixer()]
         }
+    },
+    define:{
+        "process.env": process.env
     }
 
 })
